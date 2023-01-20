67db78ee2ef1cd6b171fc60ad3f416eb733d9d5f
SRK-prog <srksivaram262@gmail.com>
2023-01-20 23:56:06 +0530

    build

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "http://ec2-3-110-86-150.ap-south-1.compute.amazonaws.com:8080/api",
 });
